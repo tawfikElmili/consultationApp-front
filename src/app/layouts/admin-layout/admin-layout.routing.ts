50ea8e4351075184246f388a60bfa90b901969c5
Tawfik ELMILI <tawfik.elmili@altran.com>
2022-04-28 14:50:23 +0100

    delete tables

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,12 +5,11 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
 
 export const AdminLayoutRoutes: Routes = [
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'user-list',   component: UserListComponent },
-    { path: 'consultation-list',component: ConsultationListComponent },
-    { path: 'consultation-add',component: ConsultationAddComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'user-profile', component: UserProfileComponent },
+    { path: 'user-list', component: UserListComponent },
+    { path: 'consultation-list', component: ConsultationListComponent },
+    { path: 'consultation-add', component: ConsultationAddComponent },
 ];
